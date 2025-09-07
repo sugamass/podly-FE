c4ea6865de4e50391a232c0171ffce3244968bb2
sugamass <sugamaron10@gmail.com>
2025-09-07 15:34:35 +0900

    fix babel and metro

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,13 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      "babel-preset-expo",
+      [
+        "babel-preset-expo",
+        {
+          jsxRuntime: "automatic",
+          jsxImportSource: "react-native-css-interop",
+        },
+      ],
       "nativewind/babel",
     ],
     plugins: [
--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,15 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Fix for react-native-url-polyfill ESModule import issues
+config.resolver.unstable_enableSymlinks = true;
+config.resolver.unstable_enablePackageExports = true;
+
+// Fix for react-native-css-interop jsx-runtime resolution
+config.resolver.alias = {
+  ...config.resolver.alias,
+  'react-native-css-interop/jsx-runtime': 'react-native-css-interop/dist/runtime/jsx-runtime',
+  'react-native-css-interop/jsx-dev-runtime': 'react-native-css-interop/dist/runtime/jsx-dev-runtime',
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
