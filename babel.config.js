9aa7bf21dfa44bd13c18b31bd7ec9d84fce1477c
sugamass <sugamaron10@gmail.com>
2025-09-07 17:01:44 +0900

    Revert "fix babel and metro"
    This reverts commit c4ea6865de4e50391a232c0171ffce3244968bb2.

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      [
-        "babel-preset-expo",
-        {
-          jsxRuntime: "automatic",
-          jsxImportSource: "react-native-css-interop",
-        },
-      ],
+      "babel-preset-expo",
       "nativewind/babel",
     ],
     plugins: [
